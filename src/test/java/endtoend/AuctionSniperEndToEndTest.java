2ae38052cac17390a563edd17d022601f4cce5c6
Fabio Luiz Nery de Miranda <fabio@miranti.net.br>
2011-05-21 05:42:26 -0300

    Minimum FakeAuctionServer setup

--- src/test/java/endtoend/AuctionSniperEndToEndTest.java
+++ src/test/java/endtoend/AuctionSniperEndToEndTest.java
@@ -4,12 +4,12 @@ import org.junit.After;
 import org.junit.Test;
 
 public class AuctionSniperEndToEndTest {
-	// private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+	private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
 	private final ApplicationRunner application = new ApplicationRunner();
 	
 	@Test
 	public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
-		// auction.startSellingItem(); // Step 1
+		auction.startSellingItem(); // Step 1
 		// application.startBiddingIn(auction); // Step 2
 
 		application.startBidding(); // Just setting up Swing and WindowLicker
@@ -22,7 +22,7 @@ public class AuctionSniperEndToEndTest {
 	// Additional cleanup
 	@After
 	public void stopAuction() {
-		// auction.stop();
+		auction.stop();
 	}
 	
 	@After 
