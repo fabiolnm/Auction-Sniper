6e2e9b7a39719f56cc9d095cc97fd2244bfa5da4
Fabio Luiz Nery de Miranda <fabio@miranti.net.br>
2011-05-21 05:07:37 -0300

    Joining message while waiting a connection implemented and tests passed.

--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -1,6 +1,7 @@
 package auctionsniper.ui;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MainWindow extends JFrame {
 	public static final String NAME = "Auction Sniper Main";
@@ -9,11 +10,20 @@ public class MainWindow extends JFrame {
 	public static final String SNIPER_STATUS_NAME = "sniper_status";
 	public static final String STATUS_JOINING = "Joining";
 	
+	private final JLabel sniperStatus = createLabel(STATUS_JOINING);
+	
 	public MainWindow() {
 		super(TITLE);
 		setName(NAME);
+		add(sniperStatus);
+		pack(); // fit to prefered size
 		setVisible(true);
-		setSize(300, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private JLabel createLabel(String initialText) {
+		JLabel result = new JLabel(initialText);
+		result.setName(SNIPER_STATUS_NAME);
+		return result;
 	}
 }
