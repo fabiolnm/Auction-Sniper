7506dcf703392905b9db3ffab6ddee204e0d21ab
Fabio Luiz Nery de Miranda <fabio@miranti.net.br>
2011-06-05 01:23:51 -0300

    - Add commons-lang dependency and toString(), equals() and hashCode() methods using commons-lang. (now AuctiionSniperTests passes).

--- src/main/java/auctionsniper/SniperState.java
+++ src/main/java/auctionsniper/SniperState.java
@@ -1,5 +1,9 @@
 package auctionsniper;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 public class SniperState {
 	public final String itemId;
 	public final int lastPrice, lastBid;
@@ -8,5 +12,20 @@ public class SniperState {
 		this.itemId = itemId;
 		this.lastPrice = lastPrice;
 		this.lastBid = lastBid;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return EqualsBuilder.reflectionEquals(this, other);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 }
