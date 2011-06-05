79293255faf1c6b9f4aaeeb6d9fda6c4ae12637e
Fabio Luiz Nery de Miranda <fabio@miranti.net.br>
2011-06-05 20:27:28 -0300

    Introduces SnipersTableModel

--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -1,11 +1,9 @@
 package auctionsniper.ui;
 
-import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.SwingUtilities;
+import javax.swing.table.AbstractTableModel;
 
 import auctionsniper.SniperSnapshot;
 
@@ -21,35 +19,55 @@ public class MainWindow extends JFrame {
 	public static final String STATUS_WINNING = "Winning";
 	public static final String STATUS_WON = "Won";
 	
-	public static final String[] STATUS_TEXT = {
-		STATUS_JOINING, STATUS_BIDDING, STATUS_WINNING, STATUS_LOST, STATUS_WON
-	};
-	
-	private final JTable sniperTable = createSniperTable();
+	private final SnipersTableModel snipers = new SnipersTableModel();
 	
 	public MainWindow() {
 		super(TITLE);
 		setName(NAME);
-		add(new JScrollPane(sniperTable));
+		add(new JScrollPane(createSniperTable()));
 		pack(); // fit to prefered size
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	private JTable createSniperTable() {
-		JTable table = new JTable(1,4);
+		JTable table = new JTable(snipers);
 		table.setName(SNIPER_TABLE_NAME);
 		return table;
 	}
 
-	public void showState(final SniperSnapshot state) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				sniperTable.setValueAt(state.itemId, 0, 0);
-				sniperTable.setValueAt(state.lastPrice, 0, 1);
-				sniperTable.setValueAt(state.lastBid, 0, 2);
-				sniperTable.setValueAt(STATUS_TEXT[state.status.ordinal()], 0, 3);
+	public void showState(final SniperSnapshot snapshot) {
+		snipers.updateSnapshot(snapshot);
+	}
+	
+	static class SnipersTableModel extends AbstractTableModel {
+		private static final String[] STATUS_TEXT = {
+			STATUS_JOINING, STATUS_BIDDING, STATUS_WINNING, STATUS_LOST, STATUS_WON
+		};
+		
+		private SniperSnapshot snapshot;
+		
+		public int getRowCount() {
+			return 1;
+		}
+
+		public int getColumnCount() {
+			return 4;
+		}
+
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			switch (columnIndex) {
+			case 0: return snapshot.itemId;
+			case 1: return snapshot.lastPrice;
+			case 2: return snapshot.lastBid;
+			case 3: return STATUS_TEXT[snapshot.status.ordinal()];
 			}
-		});
+			throw new IllegalArgumentException(String.format("Invalid column index [%s]", columnIndex));
+		}
+		
+		public void updateSnapshot(SniperSnapshot snapshot) {
+			this.snapshot = snapshot;
+			fireTableRowsUpdated(0, 0);
+		}
 	}
 }
