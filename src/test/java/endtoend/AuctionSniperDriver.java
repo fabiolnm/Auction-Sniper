0617d157224f5a5d929df8410ba228d1f98f514d
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2011-07-17 02:39:59 -0300

    AuctionSniperDriver focus on MainWindow at startup, to avoid selectors failing due to be focused in a wrong window.

--- src/test/java/endtoend/AuctionSniperDriver.java
+++ src/test/java/endtoend/AuctionSniperDriver.java
@@ -3,12 +3,14 @@ package endtoend;
 import static org.hamcrest.Matchers.equalTo;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.table.JTableHeader;
 
 import auctionsniper.ui.MainWindow;
 
 import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.ComponentSelector;
 import com.objogate.wl.swing.driver.JButtonDriver;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
@@ -25,21 +27,27 @@ public class AuctionSniperDriver extends JFrameDriver {
 		System.setProperty("com.objogate.wl.keyboard", "US");
 	}
 	
+	// topLevelFrame: a ComponentFinder that expects to find a single GUI component.
+	// named and showingOnScreen are WindowLicker matchers, 
+	// to describe user interface components that are searched for by ComponentFinders
+	private static final ComponentSelector<JFrame> windowSelector = 
+		topLevelFrame(named(MainWindow.NAME), showingOnScreen());
+	
 	public AuctionSniperDriver(int timeout) {
 		// A Gesture interacts with the user interface through the input devices. 
 		// Window Licker runs tests by performing Gestures and using Probes to observe the results.
-		super(new GesturePerformer(), 
-			// topLevelFrame: a ComponentFinder that expects to find a single GUI component.
-			// named and showingOnScreen are WindowLicker matchers, 
-			// to describe user interface components that are searched for by ComponentFinders
-			topLevelFrame(named(MainWindow.NAME), showingOnScreen()),
-			
+		super(new GesturePerformer(), windowSelector,
 			// A Probe samples the system and reports whether the last sample satisfies some test criteria. 
 			// Probes are run asynchronously by a Prober, which is responsible for inserting the probe into 
 			// the system under test, synchronising with its execution and reporting the result of the probe.
 			new AWTEventQueueProber(timeout, 100));
 
 		hasTitle(MainWindow.TITLE);
+		focusOnWindow();
+	}
+
+	private void focusOnWindow() {
+		new JFrameDriver(this, windowSelector).leftClickOnComponent();
 	}
 
 	public void hasColumnTitles() {
